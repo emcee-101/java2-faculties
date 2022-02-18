86a674f1aae36b979d027a65ea5398de54480cfc
Niklas Herzog <niklas.herzog@fh-erfurt.de>
2022-02-18 15:42:20 +0100

    New structure and initial thoughts on Lists and Repository

--- faculty-service/src/main/java/de/fherfurt/faculty/data/Controller.java
+++ faculty-service/src/main/java/de/fherfurt/faculty/data/Controller.java
@@ -1,11 +1,12 @@
 
 
 public class Controller {
+
+
     // Listen
     List<University> universities;
     List<Faculty> faculties;
     List<Course> courses;
     List<Modules> modules;
-    // Funktionalitäten
-    // (insert, update, delete)
+
 }
