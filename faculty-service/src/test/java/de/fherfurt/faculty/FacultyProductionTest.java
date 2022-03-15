af44debeb68e42edd5ce374c4283d19521e9ec62
Niklas Herzog <niklas.herzog@fh-erfurt.de>
2022-03-15 17:43:55 +0100

    Reinserting @Falko's Test after i deleted them mistakenly (sorry 🌞)

--- faculty-service/src/test/java/de/fherfurt/faculty/FacultyProductionTest.java
+++ faculty-service/src/test/java/de/fherfurt/faculty/FacultyProductionTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import java.util.List;
+
 class FacultyProductionTest {
 
     UniversityRepository universityRepository;
@@ -94,4 +96,37 @@ class FacultyProductionTest {
         assertTrue(resultTest1 == (testData.getFaculties().get(0).getDecanName()));
         assertTrue(resultTest2 == "Faculty not found");
     }
+
+    @Test
+    void filterModulesBySemesterAndCourse() {
+        // given
+        String testModuleName = testData.getModules().get(0).getName();
+        int testModuleSemester = testData.getModules().get(0).getSemester();
+        String testCourseName = testData.getModules().get(0).getCourseName();
+        // when
+        List<String> testOutput = facultyProduction.filterModulesBySemesterAndCourse(testCourseName, testModuleSemester);
+        List<String> testOutput2 = facultyProduction.filterModulesBySemesterAndCourse(testCourseName, testModuleSemester + 1);
+        List<String> testOutput3 = facultyProduction.filterModulesBySemesterAndCourse(testCourseName + "test", testModuleSemester);
+        // then
+        assertTrue(testOutput.contains(testModuleName));
+        assertFalse(testOutput2.contains(testModuleName));
+        assertFalse(testOutput3.contains(testModuleName));
+
+
+    }
+
+    @Test
+    void filterModulesByCourse() {
+        // given
+        String testModuleName = testData.getModules().get(0).getName();
+        String testCourseName = testData.getModules().get(0).getCourseName();
+        // when
+        List<String> testOutput = facultyProduction.filterModulesByCourse(testCourseName);
+        List<String> testOutput2 = facultyProduction.filterModulesByCourse(testCourseName + "test");
+        // then
+        assertTrue(testOutput.contains(testModuleName));
+        assertFalse(testOutput2.contains(testModuleName));
+    }
+
+
 }
