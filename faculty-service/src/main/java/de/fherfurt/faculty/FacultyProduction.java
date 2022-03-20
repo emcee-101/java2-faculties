50e708935082c765529042488da77888db15026c
fa2476ku <mnHAEA7Bk8Mb>
2022-03-20 19:45:09 +0100

    added missing in-code documentation

--- faculty-service/src/main/java/de/fherfurt/faculty/FacultyProduction.java
+++ faculty-service/src/main/java/de/fherfurt/faculty/FacultyProduction.java
@@ -36,6 +36,16 @@ public class FacultyProduction {
 
 
 // CODE HIER!!!!
+
+    /*
+     *
+     * filters an inputModuleList with specified parameters and returns the names of fitting modules in an outputModuleList
+     *
+     * @param courseName name of the course in which the module takes place
+     * @param numberOfSemester the semester in which the module takes place
+     *
+     */
+
     public List<String> filterModulesBySemesterAndCourse (String courseName, int numberOfSemester) {
         List<Module> inputModuleList;
         inputModuleList = moduleRepository.getList();
@@ -48,6 +58,14 @@ public class FacultyProduction {
         }
         return outputModuleList;
     }
+
+    /*
+     *
+     * filters an inputModuleList with a specified parameter and returns the names of fitting modules in an outputModuleList
+     *
+     * @param courseName name of the course in which the module takes place
+     *
+     */
 
     public List<String> filterModulesByCourse (String courseName) {
         List<Module> inputModuleList;
